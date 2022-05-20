8100f78bd82a1216af294ed63e9ba1454f5ba89b
thanhphuongIT <thanhphuong468@gmail.com>
2022-05-20 20:07:46 +0700

    feat: code base

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { InputText } from "primereact/inputtext";
+import { Checkbox } from "primereact/checkbox";
+import { Button } from "primereact/button";
+import "./App.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-content-center">
+      <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6 border-2">
+        <div className="text-center mb-5">
+          <img
+            src="assets/images/blocks/logos/hyper.svg"
+            alt="hyper"
+            height={50}
+            className="mb-3"
+          />
+          <div className="text-900 text-3xl font-medium mb-3">Welcome Back</div>
+          <span className="text-600 font-medium line-height-3">
+            Don't have an account?
+          </span>
+          <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">
+            Create today!
+          </a>
+        </div>
+
+        <div>
+          <label htmlFor="email" className="block text-900 font-medium mb-2">
+            Email
+          </label>
+          <InputText type="text" className="w-full mb-3" />
+
+          <label htmlFor="password" className="block text-900 font-medium mb-2">
+            Password
+          </label>
+          <InputText type="password" className="w-full mb-3" />
+
+          <div className="flex align-items-center justify-content-between mb-6">
+            <div className="flex align-items-center">
+              <Checkbox id="rememberme" className="mr-2" />
+              <label htmlFor="rememberme">Remember me</label>
+            </div>
+            <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">
+              Forgot your password?
+            </a>
+          </div>
+
+          <Button label="Sign In" icon="pi pi-user" className="w-full" />
+        </div>
+      </div>
     </div>
   );
 }
